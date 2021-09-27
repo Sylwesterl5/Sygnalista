00b077fac0244674954dd36bface922b366dc8bb
Sylwesterl5 <sylwesterl5271@gmail.com>
2021-09-27 15:08:14 +0200

    next builid  list wrapper

--- FrontEndSygnalista/src/components/Listwrapper/ListItem/ListItem.js
+++ FrontEndSygnalista/src/components/Listwrapper/ListItem/ListItem.js
@@ -3,7 +3,15 @@ import './ListItem.css';
 
 const ListItem = () => (
     <li>
-        item
+        <div className='listItem__list'>
+            <div className='listItem__wrapper'>
+                <div className='listItem__status'></div>
+                <h2 className='listItem__Title'>nazwaza głoszenia</h2>
+                <p className='listItem__data'>09.23.1200</p>
+            </div>
+            <p className='listItem__description'>opfasdasdsfjkasjgsiodfjgisjdfgjos</p>
+            <button className='listItem__button'>otwórz</button>
+        </div>
     </li>
 );
 
