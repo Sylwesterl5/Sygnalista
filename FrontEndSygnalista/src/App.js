e0b6828ae25dd1ebde9a030125aaa9eb818d4991
Sylwesterl5 <sylwesterl5271@gmail.com>
2021-09-27 12:17:10 +0200

    start to add list Wrapper

--- FrontEndSygnalista/src/App.js
+++ FrontEndSygnalista/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import ListWrapper from './components/Listwrapper/ListWrapper';
 
 function App() {
   return (
     <div className="App">
+      <ListWrapper/>
     </div>
   );
 }
---
+++ FrontEndSygnalista/src/components/Listwrapper/ListWrapper.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListItem from './ListItem/ListItem';
+
+
+const ListWrapper = () => (
+    <ul>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+    </ul>
+)
+
+export default ListWrapper;
