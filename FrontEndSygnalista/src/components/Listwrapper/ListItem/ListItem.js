2c813c345c0c4f8659255300aedf78e1ed133d85
Sylwesterl5 <sylwesterl5271@gmail.com>
2021-09-29 15:07:12 +0200

    to end list application

--- FrontEndSygnalista/src/components/Listwrapper/ListItem/ListItem.js
+++ FrontEndSygnalista/src/components/Listwrapper/ListItem/ListItem.js
@@ -1,15 +1,15 @@
 import React from "react";
 import './ListItem.css';
 
-const ListItem = () => (
+const ListItem = (props) => (
     <li>
         <div className='listItem__list'>
             <div className='listItem__wrapper'>
-                <div className='listItem__status'></div>
-                <h2 className='listItem__Title'>nazwaza głoszenia</h2>
-                <p className='listItem__data'>09.23.1200</p>
+                <div className='listItem__status'> {props.status}</div>
+                <h2 className='listItem__Title'>{props.title}</h2>
+                <p className='listItem__data'>{props.data}</p>
             </div>
-            <p className='listItem__description'>opfasdasdsfjkasjgsiodfjgisjdfgjos</p>
+            <p className='listItem__description'>{props.description}</p>
             <button className='listItem__button'>otwórz</button>
         </div>
     </li>
--- FrontEndSygnalista/src/components/Listwrapper/ListWrapper.js
+++ FrontEndSygnalista/src/components/Listwrapper/ListWrapper.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
 import './ListWrapper.css';
+import { listApplications } from '../../data/ListApplications';
 
 
 const ListWrapper = () => (
     <ul className='listWrapper__Wrapper'>
-        <ListItem/>
+        { listApplications.map(item =>(
+            <ListItem
+                status = {item.status}
+                title = {item.title}
+                data = {item.data}
+                description = {item.description}
+            />
+        ))}
     </ul>
 )
 
